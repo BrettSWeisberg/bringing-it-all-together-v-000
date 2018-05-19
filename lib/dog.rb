54e5530dc6bb1ceb6907119505e0e63919a76870
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-19 18:33:45 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -72,7 +72,7 @@ class Dog
     binding.pry
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?")
     if !dog.empty?
-    
+
       dog_data = dog[0]
       dog = Dog.new(name:dog_data[1],breed:dog_data[2],id:dog_data[0])
 
