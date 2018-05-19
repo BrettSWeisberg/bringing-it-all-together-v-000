dcc0422825fde4809ada6be726317eae4659cf00
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-19 18:33:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,7 +74,7 @@ class Dog
       binding.pry
       dog_data = dog[0]
       dog = Dog.new(name:dog_data[1],breed:dog_data[2],id:dog_data[0])
-    
+
     else
       dog = self.create(name:@name,breed:@breed)
     end
