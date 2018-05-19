ef9f51bb69501750b6f3494b6f3221dfa007d3b7
Brett Weisberg <weisberg.brett@gmail.com>
2018-05-19 18:31:41 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,4 +68,15 @@ class Dog
     dog
   end
 
+  def self.find_or_create_by(name:,breed:)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?"
+    if !dog.empty?
+      dog_data = dog[0]
+      dog = Dog.new(name:dog_data[1],breed:dog_data[2],id:dog_data[0])
+    else
+      dog = self.create(name:@name,breed:@breed)
+    end
+    dog
+  end
+
 end
